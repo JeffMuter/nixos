ffb23bb8ff58323c5ebb6d61d05466e16bdf0887
System administrator <root@nixos.main.wasserstrom.com>
2025-01-04 10:52:16 +0000

    added backups for clipboard integration for terminal when not in tmux

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
 { config, lib, pkgs, ... }:
 
 {
+  # sets the version/channel of Nix i want to use
   system.stateVersion = "24.11"; 
   system.autoUpgrade.channel = "https://channels.nixos.org/nixos-24.11";
 
@@ -16,6 +10,7 @@
     <nixos-wsl/modules>
   ];
 
+  # settings to enable and default user if using wsl
   wsl.enable = true;
   wsl.defaultUser = "emerald";
 
@@ -39,17 +34,11 @@
     nodejs
     pkgs.stylua
     unzip
+    xclip # only necessary if outside tmux
+    wl-clipboard #backup if xclip isn't working
     sqlc
     postgresql
     sqlite
     tailwindcss
   ];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-#  system.stateVersion = "24.05"; # Did you read the comment?
 }
